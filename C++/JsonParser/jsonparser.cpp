cb6eb6220e0dee3fc495fa9da849d430d4c31fb7
KarenVardumyan <karenvardumyan94@gmail.com>
2019-03-24 21:17:43 +0400

    Updated json parser

--- C++/JsonParser/jsonparser.cpp
+++ C++/JsonParser/jsonparser.cpp
@@ -3,31 +3,104 @@
 #include <fstream>
 #include <streambuf>
 
-
 class Person {
+
     std::string name;
     int age;
     std::string car;
-
+    
     public:
+
     std::string get_name (void) {
         return this -> name;
     }
+
     void set_name (std::string name) {
         this -> name = name;
     }
+
+    int get_age (int age) {
+        return this -> age;
+    }
+
+    void set_age (int age){
+        this -> age = age;
+    }
+
+    std::string get_car (std::string car) {
+        return this -> car;
+    }
+
+    void set_car (std::string car) {
+        this -> car = car;
+    }
+
+    Person (void) {
+        this -> name = "person";
+        this -> age = 18;
+        this -> car = "car";
+    }
+
+    Person (std::string name, int age, std::string car) {
+        this -> name = name;
+        this -> car = car;
+        this -> age = age;
+    }
+
+    ~Person (void) {
+        // std::cout<<"Person destructor called" <<std::endl;
+    }
+
+    void print (void) {
+        std::cout<< "Name : " << this -> name <<std::endl;
+        std::cout<< "Age : " << this -> age <<std::endl;
+        std::cout<< "Car : " << this -> car <<std::endl;
+    }
+
 };
 
+std::string get_firs_object (std::string json) {
+    std::string object = "";
+    for(int i = 0; i < json.length(); ++i) {
+        if(json[i] == '{') {
+            for(int j = i + 1; j < json.length(); ++j) {
+                if(json[j] == '}') {
+                    return object;
+                }
+                object = object + json[j];
+            }
+        }
+    }
+}
+
+void remove_first_object (std::string& json) {
+    std::string newJson = "[";
+    for(int i = 1; i < json.length(); ++i) {
+        if(json[i] == '}') {
+            for(int j = i + 2; j < json.length(); ++j) {
+                newJson = newJson + json[j];
+            }
+            json = newJson;
+            return;
+        }
+    }
+}
 
 int main(){
     std::ifstream t("file.json");
-    std::string str((std::istreambuf_iterator<char>(t)),
+    std::string json((std::istreambuf_iterator<char>(t)),
     std::istreambuf_iterator<char>());
     
+    std::string json_array [4];
 
-    Person armen;
+    for(int i = 0; i < 4; ++i) {
+        json_array[i] = get_firs_object(json);
+        remove_first_object(json);
+    }
     
-    std::cout<< "Json file \n" << str <<std::endl;
+    for(int i = 0; i < 4; ++i) {
+        std::cout<< json_array[i] <<std::endl;
+    }
 
     return 0;
 }
